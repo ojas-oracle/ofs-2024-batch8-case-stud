571aafc8c47266577f69c1e4fa4bf86575702453
ojas-oracle <ojas@oracle.com>
2024-08-14 14:17:12 +0530

    fixes for txn , added swagger

---
+++ backend/src/main/java/com/ofss/main/inb/controller/AdminController.java
@@ -0,0 +1,28 @@
+// package com.ofss.main.inb.controller;
+
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import com.ofss.main.inb.service.AdminService;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+
+
+// @RestController
+// @RequestMapping("admin")
+// public class AdminController {
+
+//     @Autowired
+//     AdminService adminService;
+    
+//     @PostMapping("path")
+//     public String postMethodName(@RequestBody String entity) {
+//         //TODO: process POST request
+        
+//         return entity;
+//     }
+    
+
+// }
--- backend/src/main/java/com/ofss/main/inb/service/TransactionServiceImpl.java
+++ backend/src/main/java/com/ofss/main/inb/service/TransactionServiceImpl.java
@@ -2,7 +2,10 @@ package com.ofss.main.inb.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ofss.main.inb.domain.Account;
 import com.ofss.main.inb.domain.Transaction;
+import com.ofss.main.inb.repo.AccountRepo;
 import com.ofss.main.inb.repo.TransactionRepo;
 
 
@@ -12,8 +15,21 @@ public class TransactionServiceImpl implements TransactionService{
     @Autowired
     TransactionRepo transactionRepo;
 
+    @Autowired
+    AccountRepo accountRepo;
+
     @Override
     public Transaction createTxn(Transaction transaction) {
+        Account fromAccount = accountRepo.findById(transaction.getFrom_account().getAccountNumber()).get();
+        Account toAccount = accountRepo.findById(transaction.getTo_account().getAccountNumber()).get();
+        if(fromAccount.getBalance() < transaction.getAmount()){
+            System.out.println("Account balance not sufficient");
+            return null;
+        }
+        fromAccount.setBalance(fromAccount.getBalance() - transaction.getAmount());
+        toAccount.setBalance(toAccount.getBalance() + transaction.getAmount());
+        accountRepo.save(fromAccount);
+        accountRepo.save(toAccount);
         return transactionRepo.save(transaction);
     }
     
